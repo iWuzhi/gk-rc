d1a0d0ebb96ff0f891b4e3612287ca65b2a2d0be
iWuzhi <2461358597@qq.com>
2020-12-25 19:52:24 +0800

    init: Add tsc script

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,19 @@
 /**
- * @author iWuzhi
- * @date 2020-12-20 17:50:09
+ * author iWuzhi
+ * date 2020-12-25 12:13:18
  */
 
-export { Button as default } from '@material-ui/core';
+import React from 'react';
+
+import { Button as MUButton } from '@material-ui/core';
+
+import { useStyles } from './style';
+
+interface IProps {}
+
+const Button: React.FC<IProps> = ({ children, ...props}) => {
+  const styles = useStyles();
+  return <MUButton className={styles.font} {...props}>{children}</MUButton>
+}
+
+export default Button;
