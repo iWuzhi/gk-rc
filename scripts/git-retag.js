9413b1bdd54c2785ea682a0ea7a564fe0a785d1e
iWuzhi <2461358597@qq.com>
2021-01-01 17:07:53 +0800

    fix: gitflow err

--- scripts/git-retag.js
+++ scripts/git-retag.js
@@ -5,6 +5,5 @@ const child_process = require('child_process');
 
 const packageJSON = require('../package.json');
 
-child_process.spawnSync(`git tag -d v${packageJSON.version}`);
-child_process.spawnSync(`git push origin :refs/tags/v${packageJSON.version}`);
-child_process.spawnSync(`git tag v${packageJSON.version}`);
+child_process.execSync(`git tag -d v${packageJSON.version}`);
+child_process.execSync(`git tag v${packageJSON.version}`);
